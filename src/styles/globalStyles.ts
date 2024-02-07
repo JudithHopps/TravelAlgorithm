68f96af9d568451cb6d27d75d5dafb20bd73ff34
JudithHopps <kookjeayon100@gmail.com>
2024-02-07 23:16:14 +0900

    styles : Add globalStyles.ts

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Common = {
+  MainDiv: styled.div`
+    width: 100vw;
+    height: 100vh;
+  `,
+  MarginDiv: styled.div`
+    margin: 40px;
+  `,
+  PaddingDiv: styled.div`
+    padding: 40px;
+  `,
+};
+
+export default Common;
