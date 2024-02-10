dac114398919038fd24f603e76580989f70952af
JudithHopps <kookjeayon100@gmail.com>
2024-02-10 16:07:28 +0900

    feat : button Link 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,27 @@ import ViewSource from "../components/view-source";
 import main from "../public/main.png";
 import styles from "./page.module.css";
 import Button from "../components/button";
+import Link from "next/link";
 
-const Home = () => (
-  <div className={styles.home}>
-    <ViewSource pathname="app/page.tsx" />
-    <div className={styles.imageContainer}>
-      <Image
-        alt="main"
-        src={main}
-        layout="fill"
-        sizes="100vw"
-        priority
-        className={styles.image}
-      />
-
-      <Button />
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <ViewSource pathname="app/page.tsx" />
+      <div className={styles.imageContainer}>
+        <Image
+          alt="main"
+          src={main}
+          layout="fill"
+          sizes="100vw"
+          priority
+          className={styles.image}
+        />
+        <Link href="/test">
+          <Button />
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
